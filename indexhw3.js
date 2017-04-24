ad8cda13b1fba6ecf7d77ad3f2cdb667afaa31c0
gillilands19 <gillilands19@gmail.com>
2017-04-23 17:24:50 -0700

    cleaned/commented HW3

--- indexhw3.js
+++ indexhw3.js
@@ -2,71 +2,65 @@
 
 
 const express = require('express');
-//const fs = require('fs');
-//const qs = require('querystring')
 const band = require('./lib/bandshw3.js');
-
 const app = express();
-const setRoot = {root : __dirname + '/public'};
+const setRoot = {root : __dirname + '/public'}; // set root directory sendFile method
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public')); // set the Static file location. __dirname is string that returns the current module's directory path
 app.use(require('body-parser').urlencoded({extended : true})); //used to parse form submissions that use POST method
 
+//start routes
 
-   /* function serveStaticFile(res, path, contentType, responseCode) {
-        if(!responseCode) responseCode = 200;
-        fs.readFile(__dirname + path, function(err, data) {
-            if(err) {
-                res.writeHead(500, { 'Content-Type' : 'text/plain' });
-                res.end('500 - Internal Error');
-            } else {
-                res.writeHead(responseCode, { 'Content-Type' : contentType });
-                res.end(data);
-            }
-        });
-    }*/
-
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
     res.sendFile('/home2.html', setRoot);
 });
 
-app.get('/search', function(req, res){
+app.get('/about', (req, res) => {
+    res.sendFile('/about.html', setroot)
+});
+
+app.get('/search', (req, res) => {
     res.type('text/plain');
-    let searchMatch = '';
+    let searchMatch = ''; // declare searchMatch
+    
+    //set variable to pass to getBand function based on which form values were filled out.
     if(req.query.name){ 
-        searchMatch = band.getBand(req.query.name);
+        searchMatch = band.getBand(req.query.name); 
     } else if (req.query.yearsActive){
         searchMatch = band.getBand(req.query.yearsActive);
     } else {
         searchMatch = band.getBand(req.query.genre);
     }
+    
     res.send(searchMatch);
 });
 
-app.post('/add', function(req, res){
+// add route
+app.post('/add', (req, res) =>{
     res.type('text/plain');
-    let name = req.body.name;
-    let years = req.body.yearsActive;
-    let genre = req.body.genre;
-    let addMatch = band.addBand(name, years, genre);
+    let name = req.body.name; // get value from name field
+    let years = req.body.yearsActive; //get value from years field
+    let genre = req.body.genre; // get value from genre field
+    let addMatch = band.addBand(name, years, genre); // pass form field values to addBand function
     res.send(addMatch);
 });
 
-app.post('/delete', function(req, res){
+app.post('/delete', (req, res) => {
     res.type('text/plain');
-    let name = req.body.name;
+    let name = req.body.name; // get value from name field
     let deleteMatch = band.deleteBand(name);
     res.send(deleteMatch);
 });
 
-app.use(function(req, res){
+// set 404 route
+app.use((req, res) => {
     res.type('text/plain');
     res.status(404);
     res.send('404 - Not Found');
 });
 
-app.use(function(err, req, res, next){
+app.use((err, req, res, next) => {
     console.error(err.stack);
     res.type('text/plain');
     res.status(500);
@@ -75,44 +69,7 @@ app.use(function(err, req, res, next){
 
 
 
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), () => {
     console.log( 'Express started on http://localhost:' +
                   app.get('port') + '; press Ctrl+C to terminate.');
 });
-
-/*
-http.createServer((req, res) => {
-    //normalize url by removing querystring, optional
-    // trailing slash, and making it lowercase
-    let path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
-    let splitUrl = req.url.split('?');
-    let bandParam = qs.parse(splitUrl[1]);
-    switch(path) {
-        case '':
-            serveStaticFile(res, '/public/home.html', 'text/html' );
-            break;
-        case '/about':
-            serveStaticFile(res, '/public/about.html', 'text/html');
-            break;
-        case '/search':
-            let searchMatch = band.getBand(bandParam.name);
-            res.writeHead(200, { 'Content-Type' : 'text/plain' });
-            res.end(searchMatch);
-            break;
-        case '/delete':
-            let deleteMatch = band.deleteBand(bandParam.name);
-            res.writeHead(200, { 'Content-Type' : 'text/plain' });
-            res.end(deleteMatch);
-            break;
-        case '/add':
-            let addMatch = band.addBand(bandParam.name);
-            res.writeHead(200, { 'Content-Type' : 'text/plain' });
-            res.end(addMatch);
-        default:
-            res.writeHead(404, { 'Content-Type' : 'text/plain' });
-            res.end('Not Found');
-            break;
-               }
-}).listen(3000);
-console.log('Server started on localhost:3000; Press Ctrl-C to terminate....');
-*/
