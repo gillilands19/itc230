e41ae93c7e1981f017bca13b23962ae174832164
gillilands19 <gillilands19@gmail.com>
2017-04-25 21:04:58 -0700

    linted/fixed issues

--- index.js
+++ index.js
@@ -3,53 +3,54 @@
 
 const http = require('http');
 const fs = require('fs');
-const qs = require('querystring')
+const qs = require('querystring');
 const band = require('./lib/bands.js');
 
-    function serveStaticFile(res, path, contentType, responseCode) {
-        if(!responseCode) responseCode = 200;
-        fs.readFile(__dirname + path, function(err, data) {
-            if(err) {
-                res.writeHead(500, { 'Content-Type' : 'text/plain' });
-                res.end('500 - Internal Error');
-            } else {
-                res.writeHead(responseCode, { 'Content-Type' : contentType });
-                res.end(data);
-            }
-        });
-    }
+function serveStaticFile(res, path, contentType, responseCode) {
+	if(!responseCode) responseCode = 200;
+	fs.readFile(__dirname + path, function(err, data) {
+		if(err) {
+			res.writeHead(500, { 'Content-Type' : 'text/plain' });
+			res.end('500 - Internal Error');
+		} else {
+			res.writeHead(responseCode, { 'Content-Type' : contentType });
+			res.end(data);
+		}
+	});
+}
 
 http.createServer((req, res) => {
     //normalize url by removing querystring, optional
     // trailing slash, and making it lowercase
-    let path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
-    let splitUrl = req.url.split('?');
-    let bandParam = qs.parse(splitUrl[1]);
-    switch(path) {
-        case '':
-            serveStaticFile(res, '/public/home.html', 'text/html' );
-            break;
-        case '/about':
-            serveStaticFile(res, '/public/about.html', 'text/html');
-            break;
-        case '/search':
-            let searchMatch = band.getBand(bandParam.name);
-            res.writeHead(200, { 'Content-Type' : 'text/plain' });
-            res.end(searchMatch);
-            break;
-        case '/delete':
-            let deleteMatch = band.deleteBand(bandParam.name);
-            res.writeHead(200, { 'Content-Type' : 'text/plain' });
-            res.end(deleteMatch);
-            break;
-        case '/add':
-            let addMatch = band.addBand(bandParam.name);
-            res.writeHead(200, { 'Content-Type' : 'text/plain' });
-            res.end(addMatch);
-        default:
-            res.writeHead(404, { 'Content-Type' : 'text/plain' });
-            res.end('Not Found');
-            break;
-               }
+	let path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+	let splitUrl = req.url.split('?');
+	let bandParam = qs.parse(splitUrl[1]);
+	
+	switch(path) {
+	case '':
+		serveStaticFile(res, '/public/home.html', 'text/html' );
+		break;
+	case '/about':
+			serveStaticFile(res, '/public/about.html', 'text/html');
+			break;
+		case '/search':
+			let searchMatch = band.getBand(bandParam.name);
+			res.writeHead(200, { 'Content-Type' : 'text/plain' });
+			res.end(searchMatch);
+			break;
+		case '/delete':
+			let deleteMatch = band.deleteBand(bandParam.name);
+			res.writeHead(200, { 'Content-Type' : 'text/plain' });
+			res.end(deleteMatch);
+			break;
+		case '/add':
+			let addMatch = band.addBand(bandParam.name);
+			res.writeHead(200, { 'Content-Type' : 'text/plain' });
+			res.end(addMatch);
+		default:
+			res.writeHead(404, { 'Content-Type' : 'text/plain' });
+			res.end('Not Found');
+			break;
+			   }
 }).listen(3000);
 console.log('Server started on localhost:3000; Press Ctrl-C to terminate....');
--- indexhw3.js
+++ indexhw3.js
@@ -18,56 +18,56 @@ app.set('view engine', '.html');
 //start routes
 
 app.get('/', (req, res) => {
-    res.render('home');
+	res.render('home');
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+	res.render('about');
 });
 
 app.get('/search', (req, res) => {
     
-    let searchMatch = ''; // declare searchMatch
+	let searchMatch = ''; // declare searchMatch
     
     //set variable to pass to getBand function based on which form values were filled out.
-    if(req.query.name){ 
-        searchMatch = band.getBand(req.query.name); 
-    } else if (req.query.yearsActive){
-        searchMatch = band.getBand(req.query.yearsActive);
-    } else {
-        searchMatch = band.getBand(req.query.genre);
-    }
+	if(req.query.name){ 
+		searchMatch = band.getBand(req.query.name); 
+	} else if (req.query.yearsActive){
+		searchMatch = band.getBand(req.query.yearsActive);
+	} else {
+		searchMatch = band.getBand(req.query.genre);
+	}
     
-    res.render('search', {results: searchMatch});
+	res.render('search', {results: searchMatch});
 });
 
 // add route
 app.post('/add', (req, res) =>{
-    let name = req.body.name; // get value from name field
-    let years = req.body.yearsActive; //get value from years field
-    let genre = req.body.genre; // get value from genre field
-    let addMatch = band.addBand(name, years, genre); // pass form field values to addBand function
-    res.render('add', {results: addMatch});
+	let name = req.body.name; // get value from name field
+	let years = req.body.yearsActive; //get value from years field
+	let genre = req.body.genre; // get value from genre field
+	let addMatch = band.addBand(name, years, genre); // pass form field values to addBand function
+	res.render('add', {results: addMatch});
 });
 
 app.post('/delete', (req, res) => {
-    let name = req.body.name; // get value from name field
-    let deleteMatch = band.deleteBand(name);
-    res.render('delete', {results: deleteMatch});
+	let name = req.body.name; // get value from name field
+	let deleteMatch = band.deleteBand(name);
+	res.render('delete', {results: deleteMatch});
 });
 
 // set 404 route
 app.use((req, res) => {
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 - Not Found');
+	res.type('text/plain');
+	res.status(404);
+	res.send('404 - Not Found');
 });
 
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.type('text/plain');
-    res.status(500);
-    res.send('500 - Server Error');
+	console.error(err.stack);
+	res.type('text/plain');
+	res.status(500);
+	res.send('500 - Server Error');
 });
 
 
